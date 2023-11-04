0f9472fc1e835a79a60f0090578a5a400062d6cd
Vasiliy Malyshev <malyshev.vasilii@yandex.ru>
2023-11-04 18:19:03 +0300

    04nov

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY", "default_secret_key")
-DEBUG = os.getenv("DEBUG", "False").lower() == "true"
+DEBUG = True
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "158.160.24.76").split(",")
 
 INSTALLED_APPS = [
