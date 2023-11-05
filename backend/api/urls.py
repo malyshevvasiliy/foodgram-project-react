50e3f8e7f7184f3c711991fa56ec24dddc7ee80e
Vasiliy Malyshev <malyshev.vasilii@yandex.ru>
2023-11-05 14:58:30 +0300

    05nov

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,10 @@
-from django.urls import include, path
-from rest_framework.routers import DefaultRouter
-
 from api.views import (CustomUserViewSet, IngredientViewSet, RecipeViewSet,
                        TagViewSet)
+
+from django.urls import include, path
+
+from rest_framework.routers import DefaultRouter
+
 
 router_v1 = DefaultRouter()
 router_v1.register(r"users", CustomUserViewSet, basename="users")
