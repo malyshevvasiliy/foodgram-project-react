815a83a39855df500733f8877d269c9c409a2639
Vasiliy Malyshev <malyshev.vasilii@yandex.ru>
2023-11-10 23:09:57 +0300

    10Nov

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -37,15 +37,7 @@ class CustomUserSerializer(UserSerializer):
 
     def create(self, validated_data: dict) -> User:
         """Создаёт нового пользователя."""
-        user = User(
-            email=validated_data["email"],
-            username=validated_data["username"],
-            first_name=validated_data["first_name"],
-            last_name=validated_data["last_name"],
-        )
-        user.set_password(validated_data["password"])
-        user.save()
-        return user
+        return User.objects.create_user(**validated_data)
 
 
 class SubscriptionSerializer(CustomUserSerializer):
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,7 @@
 from api.views import (CustomUserViewSet, IngredientViewSet, RecipeViewSet,
                        TagViewSet)
 
-from django.urls import include, path, re_path
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 
@@ -13,5 +13,6 @@ router_v1.register(r"recipes", RecipeViewSet, basename="recipes")
 
 urlpatterns = [
     path("", include(router_v1.urls)),
-    re_path('auth/', include("djoser.urls.authtoken")),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
