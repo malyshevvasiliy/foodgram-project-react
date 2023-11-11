9b98ff0bb91f290020d6fb891aff0e314aade524
Vasiliy Malyshev <malyshev.vasilii@yandex.ru>
2023-11-11 11:21:53 +0300

    11Nov

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -114,7 +114,7 @@ REST_FRAMEWORK = {
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
 }
 
@@ -124,6 +124,7 @@ DJOSER = {
     "PERMISSIONS": {
         "user": ("djoser.permissions.CurrentUserOrAdminOrReadOnly",),
         "current_user": ("rest_framework.permissions.IsAuthenticatedOrReadOnly",),
+        "recipes": ("rest_framework.permissions.IsAuthenticatedOrReadOnly",),
     },
     "SERIALIZERS": {
         "user": "api.serializers.CustomUserSerializer",
